febb4321b4638b59fe18efe8d3b42b83995a6d05
pcatalini <pedro.catalini@hotmail.com>
2023-12-03 09:21:43 -0300

    remove tailwind configuration optimization

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./hugo_stats.json", "./layouts/**/*.html"],
   darkMode: "class",
-  experimental: {
-    optimizeUniversalDefaults: true,
-  },
   theme: {
     extend: {
       colors: {
