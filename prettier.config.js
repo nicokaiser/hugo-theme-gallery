e92395e54e44ec6371ea3aa677149216fc58feaf
Nico Kaiser <nico@kaiser.me>
2024-02-11 15:10:30 +0100

    chore: simplify css even more

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,6 @@
 /** @type {import('prettier').Config} */
 module.exports = {
-  plugins: [
-    "prettier-plugin-go-template",
-    "prettier-plugin-organize-attributes",
-    "prettier-plugin-tailwindcss"
-  ],
+  plugins: ["prettier-plugin-go-template", "prettier-plugin-organize-attributes", "prettier-plugin-tailwindcss", "prettier-plugin-css-order"],
   printWidth: 1000,
   tabWidth: 2,
   overrides: [
