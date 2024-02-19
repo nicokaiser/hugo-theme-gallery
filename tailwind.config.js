072c513aaceec281524db580dfdc10d569001475
Nico Kaiser <nico@kaiser.me>
2024-02-19 14:51:17 +0100

    fix: use native font stack
    Instead of serving the Inter font, use the native font stack for now.
    Other fonts can be included in custom installations.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,6 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./hugo_stats.json", "./layouts/**/*.{html,js}", "./content/**/*.md", "assets/js/**/*.js"],
   darkMode: ["class", '[data-theme="dark"]'],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["InterVariable", ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
   plugins: [require("@tailwindcss/typography")],
 };
