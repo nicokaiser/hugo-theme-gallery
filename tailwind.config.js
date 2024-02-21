565013e5c99a87e98594c6d6c2a54140fb7149b3
Nico Kaiser <nico@kaiser.me>
2024-02-21 20:49:29 +0100

    feat: revert dark mode selection to class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./hugo_stats.json", "./layouts/**/*.{html,js}", "./content/**/*.md", "assets/js/**/*.js"],
-  darkMode: ["class", '[data-theme="dark"]'],
+  darkMode: ["class"],
   plugins: [require("@tailwindcss/typography")],
 };
