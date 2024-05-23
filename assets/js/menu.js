99b65adc1f3425d14d84b2a66e040b77eb96e934
salixor <salixor@pm.me>
2024-05-24 00:46:35 +0900

    Check if menu button exists before adding an event listener

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,12 +1,9 @@
 const el = document.getElementById("menu-toggle");
-el.addEventListener("click", (event) => {
-  event.preventDefault();
-  const target = document.getElementById("menu");
-  if (target.classList.contains("hidden")) {
-    target.classList.remove("hidden");
-    el.ariaExpanded = true;
-  } else {
-    target.classList.add("hidden");
-    el.ariaExpanded = false;
-  }
-});
+if (el) {
+  el.addEventListener("click", (event) => {
+    event.preventDefault();
+    const target = document.getElementById("menu");
+    el.ariaExpanded = target.classList.contains("hidden");
+    target.classList.toggle("hidden");
+  });
+}
