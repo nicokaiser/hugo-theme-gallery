46815b7715c6f5e67eb392b248b2d0d058b4c13e
ErielCruz <erielcruz@gmail.com>
2024-06-06 01:31:54 +0200

    Added download attribute to index

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -6,6 +6,8 @@ import * as params from "@params";
 const gallery = document.getElementById("gallery");
 
 if (gallery) {
+  const galleryDownload = gallery.getAttribute("image-download") === "true";
+
   const lightbox = new PhotoSwipeLightbox({
     gallery,
     children: ".gallery-item",
@@ -35,29 +37,31 @@ if (gallery) {
     errorMsg: params.errorMsg,
   });
 
-  // removed download button
-  // lightbox.on("uiRegister", () => {
-  //   lightbox.pswp.ui.registerElement({
-  //     name: "download-button",
-  //     order: 8,
-  //     isButton: true,
-  //     tagName: "a",
-  //     html: {
-  //       isCustomSVG: true,
-  //       inner: '<path d="M20.5 14.3 17.1 18V10h-2.2v7.9l-3.4-3.6L10 16l6 6.1 6-6.1ZM23 23H9v2h14Z" id="pswp__icn-download"/>',
-  //       outlineID: "pswp__icn-download",
-  //     },
-  //     onInit: (el, pswp) => {
-  //       el.setAttribute("download", "");
-  //       el.setAttribute("target", "_blank");
-  //       el.setAttribute("rel", "noopener");
-  //       el.setAttribute("title", params.downloadTitle || "Download");
-  //       pswp.on("change", () => {
-  //         el.href = pswp.currSlide.data.element.href;
-  //       });
-  //     },
-  //   });
-  // });
+  // Register the download button only if galleryDownload is true
+  if (galleryDownload) {
+    lightbox.on("uiRegister", () => {
+      lightbox.pswp.ui.registerElement({
+        name: "download-button",
+        order: 8,
+        isButton: true,
+        tagName: "a",
+        html: {
+          isCustomSVG: true,
+          inner: '<path d="M20.5 14.3 17.1 18V10h-2.2v7.9l-3.4-3.6L10 16l6 6.1 6-6.1ZM23 23H9v2h14Z" id="pswp__icn-download"/>',
+          outlineID: "pswp__icn-download",
+        },
+        onInit: (el, pswp) => {
+          el.setAttribute("download", "");
+          el.setAttribute("target", "_blank");
+          el.setAttribute("rel", "noopener");
+          el.setAttribute("title", params.downloadTitle || "Download");
+          pswp.on("change", () => {
+            el.href = pswp.currSlide.data.element.href;
+          });
+        },
+      });
+    });
+  }
 
   lightbox.on("change", () => {
     history.replaceState("", document.title, "#" + lightbox.pswp.currSlide.index);
