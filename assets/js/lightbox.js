c212e92b60135759ed92db4019009d78bdff26b1
Nico Kaiser <nico@kaiser.me>
2025-02-17 14:29:09 +0100

    feat: consider publishResources build parameter

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -20,28 +20,30 @@ if (gallery) {
     errorMsg: params.errorMsg,
   });
 
-  lightbox.on("uiRegister", () => {
-    lightbox.pswp.ui.registerElement({
-      name: "download-button",
-      order: 8,
-      isButton: true,
-      tagName: "a",
-      html: {
-        isCustomSVG: true,
-        inner: '<path d="M20.5 14.3 17.1 18V10h-2.2v7.9l-3.4-3.6L10 16l6 6.1 6-6.1ZM23 23H9v2h14Z" id="pswp__icn-download"/>',
-        outlineID: "pswp__icn-download",
-      },
-      onInit: (el, pswp) => {
-        el.setAttribute("download", "");
-        el.setAttribute("target", "_blank");
-        el.setAttribute("rel", "noopener");
-        el.setAttribute("title", params.downloadTitle || "Download");
-        pswp.on("change", () => {
-          el.href = pswp.currSlide.data.element.href;
-        });
-      },
+  if (params.enableDownload) {
+    lightbox.on("uiRegister", () => {
+      lightbox.pswp.ui.registerElement({
+        name: "download-button",
+        order: 8,
+        isButton: true,
+        tagName: "a",
+        html: {
+          isCustomSVG: true,
+          inner: '<path d="M20.5 14.3 17.1 18V10h-2.2v7.9l-3.4-3.6L10 16l6 6.1 6-6.1ZM23 23H9v2h14Z" id="pswp__icn-download"/>',
+          outlineID: "pswp__icn-download",
+        },
+        onInit: (el, pswp) => {
+          el.setAttribute("download", "");
+          el.setAttribute("target", "_blank");
+          el.setAttribute("rel", "noopener");
+          el.setAttribute("title", params.downloadTitle || "Download");
+          pswp.on("change", () => {
+            el.href = pswp.currSlide.data.element.href;
+          });
+        },
+      });
     });
-  });
+  }
 
   lightbox.on("change", () => {
     const target = lightbox.pswp.currSlide?.data?.element?.dataset["pswpTarget"];
--- exampleSite/content/_index.md
+++ exampleSite/content/_index.md
@@ -11,4 +11,7 @@ menus:
     name: Home
     weight: -1
 # sub-galleries on list pages are sorted by date and weight (descending)
+#cascade:
+#  build:
+#    publishResources: false # do not include full images. Also disable download
 ---
